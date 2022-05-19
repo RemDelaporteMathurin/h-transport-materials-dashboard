c9a9501bd3abc78854af73e5be9dbca18f73d7a6
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-19 21:09:07 +0200

    added callbacks for filters

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ layout = dbc.Container(
                                             ["tungsten", "copper", "cucrzr"],
                                             ["tungsten"],
                                             multi=True,
+                                            id="material_filter_diffusivities",
                                         ),
                                         html.Br(),
                                         html.Label("Filter by isotope:"),
@@ -66,6 +67,7 @@ layout = dbc.Container(
                                             ["H", "D", "T"],
                                             ["H"],
                                             multi=True,
+                                            id="isotope_filter_diffusivities",
                                         ),
                                         html.Br(),
                                         html.Label("Filter by author:"),
@@ -81,11 +83,12 @@ layout = dbc.Container(
                                             ["Frauenfelder", "Heinola", "Fernandez"],
                                             ["Frauenfelder"],
                                             multi=True,
+                                            id="author_filter_diffusivities",
                                         ),
                                         html.Br(),
                                         html.Label("Filter by year:"),
                                         dcc.RangeSlider(
-                                            id="year",
+                                            id="year_filter_diffusivities",
                                             min=1950,
                                             max=2021,
                                             step=1,
@@ -242,6 +245,29 @@ layout = dbc.Container(
     fluid=True,
 )
 app.layout = layout
+
+
+# callback filter material diffusivity
+@app.callback(
+    dash.Output("graph1", "figure"),
+    dash.Input("material_filter_diffusivities", "value"),
+    dash.Input("isotope_filter_diffusivities", "value"),
+    dash.Input("author_filter_diffusivities", "value"),
+    dash.Input("year_filter_diffusivities", "value"),
+)
+def update_graph(
+    material_filter_diffusivities,
+    isotope_filter_diffusivities,
+    author_filter_diffusivities,
+    year_filter_diffusivities,
+):
+    return make_diffusivities(
+        materials=material_filter_diffusivities,
+        authors=author_filter_diffusivities,
+        isotopes=isotope_filter_diffusivities,
+        years=year_filter_diffusivities,
+    )
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
