e2d4aaa71b6060fb61cf4ce0dd30fb0417299969
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-06-05 17:56:21 +0200

    added bar chart for diffusivities

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from graph import (
     add_mean_value_solubilities,
     min_year_solubilities,
     max_year_solubilities,
+    make_figure_prop_per_year
 )
 
 import h_transport_materials as htm
@@ -175,12 +176,12 @@ layout = dbc.Container(
                                         dcc.RangeSlider(
                                             id="year_filter_diffusivities",
                                             min=1950,
-                                            max=2021,
+                                            max=2022,
                                             step=1,
-                                            value=[1950, 2021],
+                                            value=[1950, 2022],
                                             marks={
                                                 int(i): str(i)
-                                                for i in np.arange(1950, 2021, step=10)
+                                                for i in np.arange(1950, 2022, step=10)
                                             },
                                             tooltip={
                                                 "placement": "bottom",
@@ -241,7 +242,7 @@ layout = dbc.Container(
                                                     materials=["tungsten"],
                                                     authors=all_authors_diffusivities,
                                                     isotopes=all_isotopes,
-                                                    years=[1950, 2021],
+                                                    years=[1950, 2022],
                                                 )
                                             ),
                                             style={"width": "150vh", "height": "70vh"},
@@ -250,6 +251,24 @@ layout = dbc.Container(
                                 ),
                             ]
                         ),
+                        dbc.Row(
+                            dbc.Col(
+                                    [
+                                        dcc.Graph(
+                                            id="graph_prop_per_year_diffusivity",
+                                            figure=make_figure_prop_per_year(
+                                                    make_diffusivities(
+                                                        materials=["tungsten"],
+                                                        authors=all_authors_diffusivities,
+                                                        isotopes=all_isotopes,
+                                                        years=[1950, 2022],
+                                                        ),
+                                                    step=5),
+                                            style={"width": "100vh", "height": "50vh"},
+                                        ),
+                                    ]
+                                )
+                            )
                     ],
                 ),
                 dcc.Tab(
@@ -306,12 +325,12 @@ layout = dbc.Container(
                                         dcc.RangeSlider(
                                             id="year_filter_solubilities",
                                             min=1950,
-                                            max=2021,
+                                            max=2022,
                                             step=1,
-                                            value=[1950, 2021],
+                                            value=[1950, 2022],
                                             marks={
                                                 int(i): str(i)
-                                                for i in np.arange(1950, 2021, step=10)
+                                                for i in np.arange(1950, 2022, step=10)
                                             },
                                             tooltip={
                                                 "placement": "bottom",
@@ -372,7 +391,7 @@ layout = dbc.Container(
                                                     materials=all_materials,
                                                     authors=all_authors_diffusivities,
                                                     isotopes=all_isotopes,
-                                                    years=[1950, 2021],
+                                                    years=[1950, 2022],
                                                 )
                                             ),
                                             style={"width": "150vh", "height": "70vh"},
@@ -455,6 +474,7 @@ def add_all_authors(n_clicks):
 # callback filter material diffusivity
 @app.callback(
     dash.Output("graph_diffusivity", "figure"),
+    dash.Output("graph_prop_per_year_diffusivity", "figure"),
     dash.Input("material_filter_diffusivities", "value"),
     dash.Input("isotope_filter_diffusivities", "value"),
     dash.Input("author_filter_diffusivities", "value"),
@@ -474,12 +494,19 @@ def update_graph(
         isotopes=isotope_filter_diffusivities,
         years=year_filter_diffusivities,
     )
+
+    all_diffusivities = make_diffusivities(
+        materials=material_filter_diffusivities,
+        authors=author_filter_diffusivities,
+        isotopes=isotope_filter_diffusivities,
+        years=[1950, 2022],
+    )
     figure = make_graph(diffusitivites)
     changed_id = [p["prop_id"] for p in dash.callback_context.triggered][0]
     if changed_id == "mean_button_diffusivity.n_clicks":
         add_mean_value(diffusitivites, figure)
 
-    return figure
+    return figure, make_figure_prop_per_year(all_diffusivities, step=5, selected_years=year_filter_diffusivities)
 
 
 @app.callback(
