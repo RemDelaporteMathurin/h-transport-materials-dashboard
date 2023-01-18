8b4e9568196e6f93a0011021d2fc0592de16394e
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-01-18 11:38:51 -0500

    fixed export to json

--- htm_dashboard/export.py
+++ htm_dashboard/export.py
@@ -3,23 +3,33 @@ import json
 from jinja2 import Template
 
 
+# TODO this is duplicated code from HTM. We could make use of the export_to_json function
 def create_data_as_dict(group: htm.PropertiesGroup):
-    data = {}
-    for property in group:
-        name = "{}_{}_{}".format(property.isotope, property.author, property.year)
-        if property.bibsource:
-            source = property.bibdata.to_string("bibtex")
-        else:
-            source = property.source
-        data_prop = {
-            "pre_exp": property.pre_exp,
-            "act_energy": property.act_energy,
-            "year": property.year,
-            "author": property.author,
-            "isotope": property.isotope,
-            "source": source,
-        }
-        data[name] = data_prop
+    keys = [
+        "material",
+        "pre_exp",
+        "act_energy",
+        "isotope",
+        "author",
+        "source",
+        "range",
+        "doi",
+        "units",
+    ]
+    data = []
+    for prop in group:
+
+        prop_dict = {key: getattr(prop, key) for key in keys if hasattr(prop, key)}
+        if "units" in prop_dict:
+            prop_dict["units"] = f"{prop_dict['units']:~}"
+        prop_dict["pre_exp"] = prop_dict["pre_exp"].magnitude
+        prop_dict["act_energy"] = prop_dict["act_energy"].magnitude
+        if prop_dict["range"]:
+            prop_dict["range"] = (
+                prop_dict["range"][0].magnitude,
+                prop_dict["range"][1].magnitude,
+            )
+        data.append(prop_dict)
     return json.dumps(data, indent=2)
 
 
