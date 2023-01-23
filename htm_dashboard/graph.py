4a558060d56a972f584b294e13bf5f2d3a1edc68
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-01-23 15:54:18 -0500

    matching authors

--- htm_dashboard/graph.py
+++ htm_dashboard/graph.py
@@ -360,9 +360,26 @@ def make_piechart_author(prop_group):
     list_of_authors = [prop.author for prop in prop_group]
     labels = np.unique(list_of_authors).tolist()
 
+    colours = []
+    list_of_prop_colours = list_of_colours(prop_group, colour_by="author")
+    for author in labels:
+        for i, prop in enumerate(prop_group):
+            if prop.author == author:
+                colours.append(list_of_prop_colours[i])
+                break
+    assert len(colours) == len(labels)
+
     values = [list_of_authors.count(isotope) for isotope in labels]
 
     labels = [lab.capitalize() for lab in labels]
 
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    fig = go.Figure(
+        data=[
+            go.Pie(
+                labels=labels,
+                values=values,
+                marker_colors=colours,
+            )
+        ]
+    )
     return fig
