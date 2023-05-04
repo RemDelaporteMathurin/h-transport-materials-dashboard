8c03feceb6fccae7b33ae68e7ea5fc88b2b8e225
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-05-04 12:17:37 -0400

    added unit test

---
+++ tests/test_units.py
@@ -0,0 +1,34 @@
+from htm_dashboard.callbacks import create_make_download_data_function
+import h_transport_materials as htm
+
+from contextvars import copy_context
+from dash._callback_context import context_value
+from dash._utils import AttributeDict
+
+from htm_dashboard.callbacks import create_make_download_data_function
+
+
+def test_export_groups():
+    """Tests the export to json callback"""
+    export_fun = create_make_download_data_function("diffusivity")
+
+    def run_callback():
+        context_value.set(
+            AttributeDict(
+                **{
+                    "triggered_inputs": [
+                        {"prop_id": "extract_button_diffusivity.n_clicks"}
+                    ]
+                }
+            )
+        )
+        return export_fun(
+            1,
+            material_filter=["tungsten"],
+            author_filter=["frauenfelder"],
+            isotope_filter=["H", "D", "T"],
+            year_filter=None,
+        )
+
+    ctx = copy_context()
+    output = ctx.run(run_callback)
