b065bb7beed825208254051fc258c04c757ec2f3
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-05-04 11:08:01 -0400

    tried wait for

--- tests/test_system.py
+++ tests/test_system.py
@@ -8,9 +8,9 @@ def test_001_child_with_0(dash_duo):
     dash_duo.start_server(app)
     # # 5. use wait_for_* if your target element is the result of a callback,
     # # keep in mind even the initial rendering can trigger callbacks
-    # dash_duo.wait_for_text_to_equal("#nully-wrapper", "0", timeout=4)
+    dash_duo.wait_for_element_by_id("graph_diffusivity", timeout=4)
     # # 6. use this form if its present is expected at the action point
-    # assert dash_duo.find_element("#nully-wrapper").text == "0"
+    assert dash_duo.find_element("graph_diffusivity").text == "0"
     # # 7. to make the checkpoint more readable, you can describe the
     # # acceptance criterion as an assert message after the comma.
     # assert dash_duo.get_logs() == [], "browser console should contain no error"
