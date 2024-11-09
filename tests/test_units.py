dc415251df38ba526c68cbda891fbd84902acee7
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-11-09 13:21:13 -0500

    fixed bug

--- tests/test_units.py
+++ tests/test_units.py
@@ -4,8 +4,12 @@ import h_transport_materials as htm
 from contextvars import copy_context
 from dash._callback_context import context_value
 from dash._utils import AttributeDict
+from datetime import datetime
 
-from htm_dashboard.callbacks import create_make_download_data_function
+from htm_dashboard.callbacks import (
+    create_make_download_data_function,
+    make_citations_graph,
+)
 
 
 def test_export_groups():
@@ -33,3 +37,10 @@ def test_export_groups():
     ctx = copy_context()
     output = ctx.run(run_callback)
     assert type(output["content"]) == str
+
+
+def test_citation_graphs_per_year_same_year():
+    current_year = datetime.now().year
+
+    group = htm.PropertiesGroup([htm.Property(year=current_year)])
+    make_citations_graph(group, per_year=True)
